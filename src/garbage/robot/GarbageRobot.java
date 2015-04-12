9abbe55322808d4a62ace387327b986e72099e41
madjer22 <maciej.nowak.zslg@gmail.com>
2015-04-12 18:11:32 +0200

    Map loaded

--- src/garbage/robot/GarbageRobot.java
+++ src/garbage/robot/GarbageRobot.java
@@ -5,9 +5,13 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
 
+//need to extend BasicGame
 public class GarbageRobot extends BasicGame
 {
+	private TiledMap map;
+	
     public GarbageRobot()
     {
         super("Garbage Robot AI");
@@ -17,8 +21,9 @@ public class GarbageRobot extends BasicGame
     {
         try
         {
+        	//set AppGameConteiner and start it
             AppGameContainer app = new AppGameContainer(new GarbageRobot());
-            app.setDisplayMode(500, 400, false);
+            app.setDisplayMode(350, 350, false);
             app.start();
         }
         catch (SlickException e)
@@ -27,17 +32,22 @@ public class GarbageRobot extends BasicGame
         }
     }
  
+    //need to override init 
     @Override
     public void init(GameContainer container) throws SlickException
     {
+    	map = new TiledMap("data/grassmap.tmx");
     }
  
+    //need to override update
     @Override
     public void update(GameContainer container, int delta) throws SlickException
     {
     }
  
+    //need to override render 
     public void render(GameContainer container, Graphics g) throws SlickException
     {
+    	map.render(15, 15);
     }
 }
