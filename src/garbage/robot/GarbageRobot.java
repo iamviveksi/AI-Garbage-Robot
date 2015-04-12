21d3ef422886463df46a78ff8e81eb28025ca5b5
madjer22 <maciej.nowak.zslg@gmail.com>
2015-04-12 18:33:50 +0200

    New map and images, sprite added

--- src/garbage/robot/GarbageRobot.java
+++ src/garbage/robot/GarbageRobot.java
@@ -1,29 +1,35 @@
 package garbage.robot;
  
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 //need to extend BasicGame
 public class GarbageRobot extends BasicGame
 {
-	private TiledMap map;
+	private TiledMap map; //map
+	private Animation sprite, up, down, left, right; //sprites
+	private float x = 64f, y = 64f; //initial position of main sprite
 	
     public GarbageRobot()
     {
+    	//text in the main window
         super("Garbage Robot AI");
     }
  
+    //start the GAME!
     public static void main(String[] arguments)
     {
         try
         {
         	//set AppGameConteiner and start it
             AppGameContainer app = new AppGameContainer(new GarbageRobot());
-            app.setDisplayMode(350, 350, false);
+            app.setDisplayMode(1050, 800, false);
             app.start();
         }
         catch (SlickException e)
@@ -32,22 +38,40 @@ public class GarbageRobot extends BasicGame
         }
     }
  
-    //need to override init 
+    //Initialize values (need to override) 
     @Override
     public void init(GameContainer container) throws SlickException
     {
-    	map = new TiledMap("data/grassmap.tmx");
+    	//load map
+    	map = new TiledMap("data/map.tmx");
+    	
+    	//set arrays images for sprites emited by keyboards events (image1, image2) to make movement effect
+    	Image [] movementUp = {new Image("data/wmg1_bk1.png"), new Image("data/wmg1_bk2.png")};
+    	Image [] movementDown = {new Image("data/wmg1_fr1.png"), new Image("data/wmg1_fr2.png")};
+    	Image [] movementLeft = {new Image("data/wmg1_lf1.png"), new Image("data/wmg1_lf2.png")};
+    	Image [] movementRight = {new Image("data/wmg1_rt1.png"), new Image("data/wmg1_rt2.png")};
+    	
+    	//draw image1 every 300ms and image2 every 300ms too
+    	int [] duration = {300, 300};
+    	
+    	//set sprites (images, durations, auto-manual-mode-refreshing)
+    	up = new Animation(movementUp, duration, false);
+    	down = new Animation(movementDown, duration, false);
+    	left = new Animation(movementLeft, duration, false);
+    	right = new Animation(movementRight, duration, false); 
+    	sprite = right; //original orientation  	
     }
  
-    //need to override update
+    //Update values (need to override)
     @Override
     public void update(GameContainer container, int delta) throws SlickException
     {
     }
  
-    //need to override render 
+    //Render values (need to override)
     public void render(GameContainer container, Graphics g) throws SlickException
     {
-    	map.render(15, 15);
+    	map.render(10, 15);
+    	sprite.draw((int)x, (int)y);
     }
 }
