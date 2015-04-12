2571045659f9ba8032350374d55a3d850711b655
madjer22 <maciej.nowak.zslg@gmail.com>
2015-04-12 18:05:29 +0200

    Main Class added + java.library.path defined

---
+++ src/garbage/robot/GarbageRobot.java
@@ -0,0 +1,43 @@
+package garbage.robot;
+ 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class GarbageRobot extends BasicGame
+{
+    public GarbageRobot()
+    {
+        super("Garbage Robot AI");
+    }
+ 
+    public static void main(String[] arguments)
+    {
+        try
+        {
+            AppGameContainer app = new AppGameContainer(new GarbageRobot());
+            app.setDisplayMode(500, 400, false);
+            app.start();
+        }
+        catch (SlickException e)
+        {
+            e.printStackTrace();
+        }
+    }
+ 
+    @Override
+    public void init(GameContainer container) throws SlickException
+    {
+    }
+ 
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException
+    {
+    }
+ 
+    public void render(GameContainer container, Graphics g) throws SlickException
+    {
+    }
+}
